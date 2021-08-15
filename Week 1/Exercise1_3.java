eae9276dd7dd9c972077a88f669c6d5b06699538
Kshitiz Kumar Singh <kshitiz.singh_cs20@gla.ac.in>
2021-08-15 11:52:22 +0530

    Programming Assignment 3 Solution

---
+++ Week 1/Exercise1_3.java
@@ -0,0 +1,27 @@
+'''Consider First n even numbers starting from zero(0).Complete the code segment to calculate sum of  all the numbers divisible by 3 from 0 to n. Print the sum.
+'''Example:'''
+'''Input: n = 5'''
+'''-------'''
+'''0 2 4 6 8'''
+'''Even number divisible by 3:0 6'''
+'''sum:6'''
+  
+  
+  
+  import java.util.Scanner;
+public class Exercise1_3 {
+       public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n=sc.nextInt();
+		int sum=0,t;
+         for(int i=0;i<2*n;i+=2){
+          if(i%3==0)
+            sum+=i;
+         }
+System.out.print(sum);
+       }
+}
+
+
+
+//  by kshitiz
