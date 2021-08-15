26c5df31040f1ab0d14e4a8de692b4c482ef42d8
Kshitiz Kumar Singh <kshitiz.singh_cs20@gla.ac.in>
2021-08-15 11:45:41 +0530

    Programming Assignment 1 Solution

---
+++ Week 1/Exercise1_1.java
@@ -0,0 +1,21 @@
+'''Complete the code segment to find the perimeter and area of a circle given a value of radius.'''
+'''You should use Math.PI constant in your program. If radius is zero or less than zero then print " please enter non zero positive number ".'''
+  
+  import java.util.Scanner;  
+public class Exercise1_1 {
+       public static void main(String[] args) {
+Scanner s = new Scanner(System.in); 
+       double radius= s.nextDouble();
+       double perimeter;
+       double area;
+	perimeter = 2 * Math.PI * radius;
+	area = perimeter * radius;
+	
+	System.out.println(perimeter);
+	System.out.println(area);
+	}
+}
+
+
+
+// by kshitiz
