14d315b4526e360cb412ad1151356d2b4a9d452f
Kshitiz Kumar Singh <kshitiz.singh_cs20@gla.ac.in>
2021-08-23 09:55:08 +0530

    Programming Assignment 3 Solution

---
+++ Week 2/Exercise2_3.java
@@ -0,0 +1,21 @@
+'''Complete the code segment to call print() method of class Question by creating a method named ‘studentMethod()’.'''
+  
+  
+public class Question23{ 
+    public static void main(String[] args) { 
+		Question23 q = new Question23();
+		q.studentMethod();
+    }
+	  void print(Question23 object){
+		System.out.print("Well Done!");
+	}
+
+ void studentMethod() {
+        Question23 q = new Question23();
+        print(q);
+    }
+}
+
+
+
+// by kshitiz
