494cce2374cab1e22398a782c702102ccbf51a06
Kshitiz Kumar Singh <kshitiz.singh_cs20@gla.ac.in>
2021-08-23 09:50:09 +0530

    Programming Assignment 1 Solution

---
+++ Week 2/Exercise2_1.java
@@ -0,0 +1,27 @@
+'''Complete the code segment to call the method  print() of class Student first and then call print() method of class School.'''
+  
+class School { 
+    public void print() { 
+		System.out.println("Hi! I class SCHOOL."); 
+    } 
+} 
+class Student { 
+	  public void print() { 
+		System.out.println("Hi! I am class STUDENT"); 
+    } 
+}
+
+public class Question21{ 
+    public static void main(String args[]){
+
+Student student = new Student();
+student.print();
+School school = new School();
+school.print();
+     } 
+}
+
+
+
+
+// by kshitiz
